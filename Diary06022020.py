77ea6dbb045856f1de2464930486ed93f4afb9b0
Dylan <dylanortizb@gmail.com>
2020-02-06 10:06:03 +0200

    20200206_10_00

---
+++ Diary06022020.py
@@ -0,0 +1,85 @@
+Python 3.7.5 (default, Nov 20 2019, 09:21:52) 
+[GCC 9.2.1 20191008] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print(4)
+4
+>>> type('Hello, World!')
+<class 'str'>
+>>> Type(17)
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    Type(17)
+NameError: name 'Type' is not defined
+>>> type(17)
+<class 'int'>
+>>> print(1,000,000)
+1 0 0
+>>> message = 'And now for something completely different'
+>>> n=17
+>>> pi=3.14159263589793
+>>> print(n)
+17
+>>> print(pi)
+3.14159263589793
+>>> type(message)
+<class 'str'>
+>>> type(n)
+<class 'int'>
+>>> type(pi)
+<class 'float'>
+>>> 76trombones ='big parade'
+SyntaxError: invalid syntax
+>>> more@1000000
+Traceback (most recent call last):
+  File "<pyshell#14>", line 1, in <module>
+    more@1000000
+NameError: name 'more' is not defined
+>>> more@ = 1000000
+SyntaxError: invalid syntax
+>>> class = 'Advanced Theoretical Zymurgy'
+SyntaxError: invalid syntax
+>>> print(1)
+1
+>>> x=2
+>>> print(x)
+2
+>>> type(pi)
+<class 'float'>
+>>> 20+32
+52
+>>> hour-1
+Traceback (most recent call last):
+  File "<pyshell#22>", line 1, in <module>
+    hour-1
+NameError: name 'hour' is not defined
+>>> hour -1
+Traceback (most recent call last):
+  File "<pyshell#23>", line 1, in <module>
+    hour -1
+NameError: name 'hour' is not defined
+>>> 20+32
+52
+>>> hour*60+minute
+Traceback (most recent call last):
+  File "<pyshell#25>", line 1, in <module>
+    hour*60+minute
+NameError: name 'hour' is not defined
+>>> minute/60
+Traceback (most recent call last):
+  File "<pyshell#26>", line 1, in <module>
+    minute/60
+NameError: name 'minute' is not defined
+>>> minute
+Traceback (most recent call last):
+  File "<pyshell#27>", line 1, in <module>
+    minute
+NameError: name 'minute' is not defined
+>>> minute=59
+>>> minute/60
+0.9833333333333333
+>>> minute=59
+>>> minute/60
+0.9833333333333333
+>>> minute//60
+0
+>>> 
---
+++ test.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+print(vars())
+a=10
+b=1.1
+c='d'
+print(vars())
